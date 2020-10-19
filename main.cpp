b0de4fc5c5626cc609a32390a1437d3d6474a1a7
GloryShovel <sk.kamil6@wp.pl>
2020-10-19 11:45:57 +0200

    First commit for Lab1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+//Structure that holds position and hit info on board
+bool board[10][10];
+
+//Functions
+int goalFunction(bool[][] board, int step) {
+  int score = 0;
+
+  //Here should be body of assesing function
+  //My idea: calculate score for each move of algorythm to asses every decision made by AI
+
+  return score;
+}
+
+int main( int argc, char** argv )
+{
+  return 0;
+}
